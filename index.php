bd26524a25e3d3be4de3616cace703136090b23e
Victor <vitocr_santos@hotmail.com>
2013-07-16 18:17:59 -0300

    Mudando titulo

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title> F </title>
+        <title> Striga </title>
     </head>
     <body>
     	<form action="index.php" method="post">
