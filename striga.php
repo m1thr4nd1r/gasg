fb87643d8d063e111c21a0c3410f39be49afc5bc
unknown <M1thr4nd1r@BLIZZARD.(none)>
2013-08-01 23:08:21 -0300

    Refatoração de variaveis

--- striga.php
+++ striga.php
@@ -4,18 +4,19 @@ include "population.php";
 
 function processIO($value)
 {
-	$population = NULL;
-    
-    $population = new Population($value);
+	$size = strlen($value);
+
+    $population = new Population($size);
     
     $population->start($value);
     
     $population->order();
     
-    $count = GA($population, $value) + 1;
+    $count = GA($population, $size) + 1;
     
     $population->pop[0]->write();
-    echo "Geracao: " . count . " Fitness: " . $population->pop[0]->fitness . "/n";
+    
+    echo "Geracao: " . $count . " Fitness: " . $population->pop[0]->fitness . "/n";
     
     echo "------------------------- /n  Configuracao Final /n";
 
@@ -24,88 +25,75 @@ function processIO($value)
     return 0;	
 }
 
-function reproduce($x,$y,$msg)
+function reproduce($parents,$size)
 {
     $child = new Node();
-    
-    $size = count($msg);
 
     $c = rand($size);
     
     for ($i = 0; $i < $size; $i++)
-        $child->text[$i] = ($i < $c+1)? $x->board[$i] : $y->board[$i]; 
+        $child->text[$i] = ($i < $c+1)? $parents[0]->board[$i] : $parents[1]->board[$i]; 
         
     return $child;
 }
 
-function random_selection($p, $X, $Y)
+function random_selection($p)
 {
-    $flag = false;
-    $percent_X = abs((float)rand()/((float)getrandmax()/100));
-    $percent_Y = abs((float)rand()/((float)getrandmax()/100));
-    
-    while (!$flag)
+    $selected = array();
+
+    while (true)
     {
-        if ($percent_X < $p->fit_percent[3])
-            $X = $p->pop[3];
-        else if ($percent_X < ($p->fit_percent[2] + $p->fit_percent[3]))  
-            $X = $p->pop[2];
-        else if ($percent_X < $p->fit_percent[1] + $p->fit_percent[2] + $p->fit_percent[3])  
-            $X = $p->pop[1];
+        $percent = abs((float)rand()/((float)getrandmax()/100));
+
+        if ($percent < $p->fit_percent[3])
+            $selected[0] = $p->pop[3];
+        else if ($percent < ($p->fit_percent[2] + $p->fit_percent[3]))  
+            $selected[0] = $p->pop[2];
+        else if ($percent < $p->fit_percent[1] + $p->fit_percent[2] + $p->fit_percent[3])  
+            $selected[0] = $p->pop[1];
         else 
-            $X = $p->pop[0];
+            $selected[0] = $p->pop[0];
         
-        if ($percent_Y < $p->fit_percent[3])
-            $Y = $p->pop[3];
-        else if ($percent_Y < ($p->fit_percent[2] + $p->fit_percent[3]))  
-            $Y = $p->pop[2];
-        else if ($percent_Y < ($p->fit_percent[1] + $p->fit_percent[2] + $p->fit_percent[3]))  
-            $Y = $p->pop[1];
+        $percent = abs((float)rand()/((float)getrandmax()/100));
+
+        if ($percent < $p->fit_percent[3])
+            $selected[1] = $p->pop[3];
+        else if ($percent < ($p->fit_percent[2] + $p->fit_percent[3]))  
+            $selected[1] = $p->pop[2];
+        else if ($percent < ($p->fit_percent[1] + $p->fit_percent[2] + $p->fit_percent[3]))  
+            $selected[1] = $p->pop[1];
         else 
-            $Y = $p->pop[0];
+            $selected[1] = $p->pop[0];
         
-        if ($X != $Y)
-            $flag = true;
+        if ($selected[0] != $selected[1])
+            return $selected;
         else
-        {
-        	$percent_Y = abs((float)rand()/((float)getrandmax()/100));
-            $percent_X = abs((float)rand()/((float)getrandmax()/100));
-        }       
+            $percent = abs((float)rand()/((float)getrandmax()/100));
     }
 }
 
-function GA($pop, $msg)
+function GA($pop, $size)
 {	
-	$gen = 0;
-	$mutationR = 0.015;
-	$popArray = array();
-	$fitnessArray = array();
-	
 	do
 	{
-		$gen++;
+		$gen = 1;
 		echo $pop->nodes[0]->text . "Geracao: " . $gen . " Fitness: " . $pop->nodes[0]->fitness . "\n";
 
-		$new_pop = new Population($msg);
-		$child = NULL;
+		$new_pop = new Population($size);
 
 		for ($i = 0; $i < Population::$size; $i++)
         {
-            $X = NULL;
-            $Y = NULL;
-            random_selection($p,$X,$Y);
-            $child = reproduce($X,$Y,$msg);
+            $parents = random_selection($p);
+            $child = reproduce($parents,$size);
             
             $new_p->pop[$i] = $child;
         }
-        
-        $size = count($msg);
 
         for ($i = 0; $i < Population::$size; $i++)
             for ($j = 0; $j < $size; $j++)
             {
                 $chance = (float) rand() / getrandmax();
-                if($chance < $mutationR)
+                if($chance < 0.015) // 0.015 = Taxa de Mutação
                     $new_p->pop[$i]->text[$j] = rand($size);
             }
         
